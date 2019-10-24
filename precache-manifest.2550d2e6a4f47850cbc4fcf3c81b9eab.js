4e8dc39a07ef03aa23678a0ef00340b0d387aced
Sasha Richi <korbutowski@gmail.com>
2019-10-24 16:49:33 +0300

    Updates

---
+++ precache-manifest.2550d2e6a4f47850cbc4fcf3c81b9eab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8b68f574e9bb6c55c63ecf75e217e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "19ce85cbed5ecf08c489",
+    "url": "/portfolio/static/css/main.e08acd0e.chunk.css"
+  },
+  {
+    "revision": "ca19aa27d7db566bbf6f",
+    "url": "/portfolio/static/js/2.ee535847.chunk.js"
+  },
+  {
+    "revision": "19ce85cbed5ecf08c489",
+    "url": "/portfolio/static/js/main.7bd42a14.chunk.js"
+  },
+  {
+    "revision": "369c41f532a8f7253c8c",
+    "url": "/portfolio/static/js/runtime-main.b6af504a.js"
+  }
+]);
