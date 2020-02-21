97907f0b3b43c4d15ab18fd43fc285e683572867
Alekskarb <korbutowski@gmail.com>
2020-02-21 20:40:51 +0300

    fade flip left

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import s from './Skills.module.css';
+import {Flip} from "react-reveal";
 
 const Skills = (props) => {
 
-        return <div className={s.skills}>
-            <div className={s.container}>
-                <span className={s.title}> My Skills </span>
-                <div className={s.skillWrap}>{props.skills.map(sk =>
-                    <div key={sk.id}
-                         className={s.block}>
+    return <div className={s.skills}>
+        <div className={s.container}>
+            <span className={s.title}> My Skills </span>
+            <Flip left>
+            <div className={s.skillWrap}>{props.skills.map(sk =>
+                <div key={sk.id}
+                     className={s.block}>
                     <div className={s.icon}>
                         <img src={sk.icon} width='100%' alt='the world is mine'/>
                     </div>
                     <h2> {sk.title} </h2>
                     <span> {sk.description} </span>
-                </div>)} </div>
+                </div>)}
             </div>
+            </Flip>
         </div>
-    }
+    </div>
+};
 export default Skills
