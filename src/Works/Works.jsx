db0ca9f9c8b1b8f57507f2cc16156a663f0a6446
Alekskarb <korbutowski@gmail.com>
2020-02-21 20:58:46 +0300

    rotate

--- src/Works/Works.jsx
+++ src/Works/Works.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import s from './Works.module.css';
+import {Rotate} from "react-reveal";
 
 const Works = (props) => {
     return (
         <div className={s.works}>
             <div className={s.container}>
                 <span className={s.title}> My Jobs </span>
-                <div className={s.wrap}>
-                    {props.work.map(w => <div className={s.block} key={w.id}>
-                    <div className={s.image}>
-                        <button className={s.buttonClick}>
-                            <a href='https://alekskarb.github.io/React_SN/'> Watch </a>
-                        </button>
+                <Rotate top left>
+                    <div className={s.wrap}>
+                        {props.work.map(w => <div className={s.block} key={w.id}>
+                            <div className={s.image}>
+                                <button className={s.buttonClick}>
+                                    <a href='https://alekskarb.github.io/React_SN/'> Watch </a>
+                                </button>
+                            </div>
+                            <span> {w.name} </span>
+                            <span> {w.description} </span>
+                        </div>)}
                     </div>
-                        <span> {w.name} </span>
-                        <span> {w.description} </span>
-                    </div>)}
-                </div>
+                </Rotate>
             </div>
         </div>
-    );
-}
+);
+};
 export default Works;
 {/*<img src={w.icon} className={s.img} alt='my job'/>*/}
