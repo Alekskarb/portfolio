c88a255179fed79477e2f9e9642bb6f1377dc4cf
Alekskarb <korbutowski@gmail.com>
2020-04-29 12:36:53 +0300

    'ground'

--- src/Works/Works.jsx
+++ src/Works/Works.jsx
@@ -7,21 +7,24 @@ const Works = (props) => {
         <div className={s.works} id='#2'>
             <div className={s.container}>
                 <span className={s.title}> My Works </span>
-                <Rotate top left>
-                    <div className={s.wrap}>
-                        {props.work.map(w => <div className={s.block} key={w.id}>
+                <div className={s.wrap}>
+                    {props.work.map(w =>
+                        <div className={s.imgWrapper} key={w.id}>
 
-                            <div className={s.image} style={{ backgroundImage: 'url('+ `${w.icon}` +')'}}>
-                            </div>
-                            <button className={s.buttonClick}>
-                                <a href={w.reference} > WATCH </a>
-                            </button>
+                                <Rotate top left>
+                                    <div className={s.image}
+                                         style={{backgroundImage: 'url(' + `${w.icon}` + ')'}}></div>
+                                </Rotate>
+                                <button className={s.buttonClick}>
+                                    <a href={w.reference}> WATCH </a>
+                                </button>
+
 
                             <span> {w.name} </span>
                             <span> {w.description} </span>
                         </div>)}
-                    </div>
-                </Rotate>
+
+                </div>
             </div>
         </div>
     );
